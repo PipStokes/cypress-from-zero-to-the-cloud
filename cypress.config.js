497c0383faa60b977cab548ce122294781b144e1
Philip Stokes <Philip.Stokes@cdl.co.uk>
2024-07-18 14:48:57 +0100

    Reconfigure cypress cloud Part2

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'f9jhe7',
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
-  projectId: "f9jhe7",
+
 })
